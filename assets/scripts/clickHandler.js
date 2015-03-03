39932b9b6d68532ae6caf18deb999ed13ee477a3
Paleolithic <tjb2597@rit.edu>
2015-03-02 23:16:52 -0500

    Add next object logic work

--- assets/scripts/clickHandler.js
+++ assets/scripts/clickHandler.js
@@ -1,65 +1,125 @@
-document.getElementById("selectSexButton").addEventListener("click", function(){
-	// nextOptionLogic(document.getElementById("selectSex"));
-	selectObj = document.getElementById("selectSex");
-	value = selectObj.options[selectObj.selectedIndex].value;
-	console.log(value);
-	if(value == "men"){
-		createNextOptions("facialHair", "Facial Hair", ["hair", "no hair"]);
-	} else if(value == "women"){
-		createNextOptions("hairColor", "Hair Color", ["blonde", "brunette"]);
-	}
-});
+// We create the HTTP Object in the function 'getHTTPObject' below	
+var myHTTPrequest = getHTTPObject();
+var xmlDoc = handleHttpResponse();
+var dateForm = document.getElementById("dateform");
 
-function toTitleCase(str)
-{
+///////// Create and get an http object (ajax)
+function getHTTPObject() {
+	var xmlhttp;
+	// Check to see if you can use native XMLHttpRequest object or ActiveX
+	if (window.XMLHttpRequest){
+		xmlhttp=new XMLHttpRequest();
+  	}
+	//  ... for IE/Windows ActiveX version
+	else if (window.ActiveXObject){
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}		/// you're using a browser that doesn't allow http object
+	else{	// return false to the variable on line 10
+		return false;
+	}
+  	/// return the object to the variable on line 10
+	return xmlhttp;	
+}
+
+//*****************************************************************
+// Create a way to look into the object  
+// In this program, there is a button that calls this function
+// See the button named 'pets' in the <body> section           
+function handleHttpResponse() {
+	
+	// first check if the http request is there
+	if (myHTTPrequest) {
+    	// good, myHTTPrequest has something (not = false)
+		myHTTPrequest.open("GET", "assets/data.xml", false);
+		myHTTPrequest.setRequestHeader("content-type", "text/xml");
+    	myHTTPrequest.send(null);
+	}
+
+  	// Now see if the request got something from the server 
+  	if (myHTTPrequest.readyState == 4) {
+    // is the server response a good one?
+    	if (myHTTPrequest.status==200) {
+      		// Use the XML DOM to unpack the XML data 
+      		xmlDoc = myHTTPrequest.responseXML; 
+	 	}
+  	}
+
+  	return xmlDoc;
+}
+
+function toTitleCase(str){
+
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-function createNextOptions(category, categoryTitle, options){
-	var dateForm = document.getElementById("dateform");
+// category, categoryTitle, options
+function createNextOptions(thisCategory){
 
-	var newLabel  = document.createElement("label");
-	newLabel.setAttribute("for", category);
-	newLabel.innerText = categoryTitle;
+	// Get div of this category and find selected value
+	divObj = document.getElementById(thisCategory);
+	selectObj = divObj.children[1];
+	inputObj = divObj.children[2];
+	inputObj.setAttribute("disabled", true);
 
-	var newSelect = document.createElement("select");
-	newSelect.id = category;
+	thisSelection = selectObj.options[selectObj.selectedIndex].value;
 
-	for (i = 0; i < options.length; i++){
-		var newOption = document.createElement("option");
-		newOption.value = options[i];
-		newOption.innerText = toTitleCase(options[i]);
-		newSelect.appendChild(newOption);
+	// Loop through xml object and find matching category
+	var selectLength = xmlDoc.getElementsByTagName("select").length;
+	var thisXMLObj;
+	for(var i = 0; i < selectLength; i++){
+		if(xmlDoc.getElementsByTagName("select")[i].getAttribute("text") == thisCategory){
+			thisXMLObj = xmlDoc.getElementsByTagName("select")[i];
+		}
 	}
 
-	var newInput = document.createElement("input");
-	newInput.type = "button";
-	newInput.id = category + "Button"; 
-	newInput.value = "Choose " + categoryTitle;
+	// Loop through the found XML object and look for the matching select element
+	// That matches the user selection
+	var thisXMLObjLength = thisXMLObj.children.length;
+	var nextXMLObj;
+	var cont = true;
+	for(var i = 0; i < thisXMLObjLength; i++ ){
+		// console.log(thisXMLObj.children[i].getAttribute("text"));
+		if(thisXMLObj.children[i].getAttribute("text") == thisSelection){
+			if(thisXMLObj.children[i].children.length > 0){
+				nextXMLObj = thisXMLObj.children[i].children[0];
+				console.log(nextXMLObj)
+				nextCategory = nextXMLObj.getAttribute("text");
+			}
+			else{ 
+				cont = false;
+				break;
+			}
+		}
+	}
 
-	// newInput.addEventListener("click", function(){
-	// 	console.log(newSelect);
-	// 	allOptions = ["", ["sex", "Sex", "men"] , ["sex", "Sex", "women"], 
-	// 	["facialHair", "Facial Hair", "hair"], ["facialHair", "Facial Hair", "no hair"], ["hairColor", "Hair Color", "blonde"], ["hairColor", "Hair Color", "brunetee"], 
-	// 	"dark", "light", "big", "small", "tall", "short", "brown", "blue"];
-	// 	value = newSelect.options[newSelect.selectedIndex].value;
-	// 	console.log(value);
-	// 	childOne = allOptions[allOptions.indexOf(value)*2 + 1];
-	// 	childTwo = allOptions[allOptions.indexOf(value)*2 + 2];
-	// 	console.log("Children: " + childOne[2] + ", " + childTwo[2]);
-	// });
+	if(cont){
+		// Create new elements
+		var newDiv 	  = document.createElement("div");
+		newDiv.setAttribute("id", nextCategory);
 
-	dateForm.appendChild(newLabel);
-	dateForm.appendChild(newSelect);
-	dateForm.appendChild(newInput);
+		var newLabel  = document.createElement("label");
+		newLabel.setAttribute("for", "select_" + nextCategory);
+		newLabel.innerText = toTitleCase(nextCategory);
+
+		var newSelect = document.createElement("select");
+		newSelect.id = nextCategory;
+
+		for (i = 0; i < nextXMLObj.children.length; i++){
+			var newOption = document.createElement("option");
+			newOption.value = nextXMLObj.children[i].getAttribute("text");
+			newOption.innerText = toTitleCase(nextXMLObj.children[i].getAttribute("text"));
+			newSelect.appendChild(newOption);
+		}
+
+		var newInput = document.createElement("input");
+		newInput.type = "button";
+		newInput.value = "Choose " + nextCategory;
+		newInput.setAttribute("onClick", "createNextOptions(this.parentNode.id)");
+
+		// Add elements to DOM
+		newDiv.appendChild(newLabel);
+		newDiv.appendChild(newSelect);
+		newDiv.appendChild(newInput);
+		dateForm.appendChild(newDiv);
+	}
 }
-
-
-// function nextOptionLogic(selectObj){
-// 	options = ["", "men", "women", "hair", "no hair", "blonde", "brunetee", "dark", "light", "big", "small", "tall", "short", "brown", "blue"];
-// 	value = selectObj.options[selectObj.selectedIndex].value
-// 	childOne = options[options.indexOf(value)*2 + 1];
-// 	childTwo = options[options.indexOf(value)*2 + 2];
-// 	console.log(selectObj.options[selectObj.selectedIndex].value);
-// 	console.log("Children: " + childOne + ", " + childTwo);
-// }
