5027fd7563566bf5bcec76ede071e20cb076ba6b
Paleolithic <tjb2597@rit.edu>
2015-03-05 03:44:40 -0500

    Ton of xml and javascript work

--- assets/scripts/clickHandler.js
+++ assets/scripts/clickHandler.js
@@ -2,6 +2,8 @@
 var myHTTPrequest = getHTTPObject();
 var xmlDoc = handleHttpResponse();
 var dateForm = document.getElementById("dateform");
+createNextOptions("class", [ "commando", "siren", "gunzerker", "assassin" ]);
+
 
 ///////// Create and get an http object (ajax)
 function getHTTPObject() {
@@ -48,78 +50,93 @@ function handleHttpResponse() {
 }
 
 function toTitleCase(str){
-
+	str = str.replace(/-/g, ' ');
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
 // category, categoryTitle, options
-function createNextOptions(thisCategory){
+function createNextOptions(thisCategory, options, selection){
+	
+	//Creates new elements based on function parameters
+	var newDiv 	  = document.createElement("div");
+	newDiv.setAttribute("id", thisCategory);
 
-	// Get div of this category and find selected value
-	divObj = document.getElementById(thisCategory);
-	selectObj = divObj.children[1];
-	inputObj = divObj.children[2];
-	inputObj.setAttribute("disabled", true);
+	var newImg = document.createElement("img");
+	// if(selection != null){	newImg.setAttribute("src", "assets/images/" + selection + ".png"); }
 
-	thisSelection = selectObj.options[selectObj.selectedIndex].value;
+	var newLabel  = document.createElement("label");
+	newLabel.setAttribute("for", "select_" + thisCategory);
+	newLabel.innerText = toTitleCase(thisCategory);
 
-	// Loop through xml object and find matching category
-	var selectLength = xmlDoc.getElementsByTagName("select").length;
-	var thisXMLObj;
-	for(var i = 0; i < selectLength; i++){
-		if(xmlDoc.getElementsByTagName("select")[i].getAttribute("text") == thisCategory){
-			thisXMLObj = xmlDoc.getElementsByTagName("select")[i];
-		}
+	var newSelect = document.createElement("select");
+	newSelect.id = thisCategory;
+
+	for (i = 0; i < options.length; i++){
+		var newOption = document.createElement("option");
+		newOption.value = options[i];
+		newOption.innerText = toTitleCase(options[i]);
+		newSelect.appendChild(newOption);
 	}
 
-	// Loop through the found XML object and look for the matching select element
-	// That matches the user selection
-	var thisXMLObjLength = thisXMLObj.children.length;
-	var nextXMLObj;
-	var cont = true;
-	for(var i = 0; i < thisXMLObjLength; i++ ){
-		// console.log(thisXMLObj.children[i].getAttribute("text"));
-		if(thisXMLObj.children[i].getAttribute("text") == thisSelection){
-			if(thisXMLObj.children[i].children.length > 0){
-				nextXMLObj = thisXMLObj.children[i].children[0];
-				console.log(nextXMLObj)
-				nextCategory = nextXMLObj.getAttribute("text");
+	var newInput = document.createElement("input");
+	newInput.type = "button";
+	newInput.value = "Choose " + toTitleCase(thisCategory);
+
+	// Add elements to DOM
+	newDiv.appendChild(newImg);
+	newDiv.appendChild(newLabel);
+	newDiv.appendChild(newSelect);
+	newDiv.appendChild(newInput);
+	dateForm.appendChild(newDiv);
+
+	newSelect.addEventListener('click', function(){
+		newImg.setAttribute("src", "assets/images/" + newSelect.options[newSelect.selectedIndex].value + ".png");
+	}, false);
+
+
+	//Input on click listener
+	newInput.addEventListener('click', function(){
+		newInput.setAttribute("disabled", true);
+		// Find appropriate select in the XML document
+		// var selectLength = xmlDoc.getElementsByTagName("select").length;
+		// var thisXMLObj;
+		// for(var i = 0; i < selectLength; i++){
+		// 	if(xmlDoc.getElementsByTagName("select")[i].getAttribute("text") == thisCategory){
+		// 		thisXMLObj = xmlDoc.getElementsByTagName("select")[i];
+		// 	}
+		// }
+
+		thisXMLObj = xmlDoc.getElementById(thisCategory);
+		
+		// Loop through the found XML object and look for the matching select element
+		// That matches the user selection
+		var selection = newSelect.options[newSelect.selectedIndex].value;
+		var thisXMLObjLength = thisXMLObj.children.length;
+		var nextXMLObj;
+		var nextCategory;
+		var cont = true;
+		for(var i = 0; i < thisXMLObjLength; i++ ){
+			if(thisXMLObj.children[i].getAttribute("text") == selection){
+				if(thisXMLObj.children[i].children.length > 0){
+					console.log("Inside if");
+					nextXMLObj = thisXMLObj.children[i].children[0];
+					nextCategory = nextXMLObj.getAttribute("text");
+					// console.log(nextXMLObj)
+				}
+				else{
+					cont = false;
+					break;
+				}
 			}
-			else{ 
-				cont = false;
-				break;
+		}
+
+		if(cont){
+			var nextOptions = [];
+			for (i = 0; i < nextXMLObj.children.length; i++){
+				nextOptions.push(nextXMLObj.children[i].getAttribute("text"));
 			}
+
+			createNextOptions(nextCategory, nextOptions, selection);			
 		}
-	}
-
-	if(cont){
-		// Create new elements
-		var newDiv 	  = document.createElement("div");
-		newDiv.setAttribute("id", nextCategory);
-
-		var newLabel  = document.createElement("label");
-		newLabel.setAttribute("for", "select_" + nextCategory);
-		newLabel.innerText = toTitleCase(nextCategory);
-
-		var newSelect = document.createElement("select");
-		newSelect.id = nextCategory;
-
-		for (i = 0; i < nextXMLObj.children.length; i++){
-			var newOption = document.createElement("option");
-			newOption.value = nextXMLObj.children[i].getAttribute("text");
-			newOption.innerText = toTitleCase(nextXMLObj.children[i].getAttribute("text"));
-			newSelect.appendChild(newOption);
-		}
-
-		var newInput = document.createElement("input");
-		newInput.type = "button";
-		newInput.value = "Choose " + nextCategory;
-		newInput.setAttribute("onClick", "createNextOptions(this.parentNode.id)");
-
-		// Add elements to DOM
-		newDiv.appendChild(newLabel);
-		newDiv.appendChild(newSelect);
-		newDiv.appendChild(newInput);
-		dateForm.appendChild(newDiv);
-	}
+	}, false); 
 }
